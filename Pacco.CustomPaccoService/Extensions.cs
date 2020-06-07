3817fc6432b014bd35a83b67259f927378a31089
Dmitry Afonchenko <undermove@yandex.ru>
2020-06-07 16:16:54 +0300

    Add custom pacco service with consul monitoring

---
+++ Pacco.CustomPaccoService/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using Convey;
+using Convey.Discovery.Consul;
+using Microsoft.AspNetCore.Builder;
+
+namespace Pacco.CustomPaccoService
+{
+    public static class Extensions
+    {
+        public static IConveyBuilder AddInfrastructure(this IConveyBuilder builder)
+        {
+            builder.AddConsul();
+            
+            return builder;
+        }
+
+        public static IApplicationBuilder UseApp(this IApplicationBuilder app)
+        {
+            app.UseConvey();
+            return app;
+        }
+    }
+}
--- Pacco.CustomPaccoService/Program.cs
+++ Pacco.CustomPaccoService/Program.cs
@@ -1,23 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Convey;
+using Convey.WebApi;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Pacco.CustomPaccoService
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+        public static async Task Main(string[] args) 
+            => await WebHost.CreateDefaultBuilder(args)
+                .ConfigureServices(services => services
+                    .AddConvey()
+                    .AddWebApi()
+                    .AddInfrastructure()
+                    .Build()
+                )
+                .Configure(app => app
+                    .UseApp())
+                .Build()
+                .RunAsync();
     }
 }
